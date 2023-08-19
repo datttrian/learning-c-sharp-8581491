0c78727e69235e5a4a4cac63cf06286effa73300
Dat T. Tran <datttrian@gmail.com>
2023-08-19 19:21:24 +0000

    Hello World C#

---
+++ Start/Overview/HelloWorld/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Hello World!");
+Console.WriteLine("What is your name?");
+
+// create a string variable and ask the user for some input
+string str = Console.ReadLine();
+Console.WriteLine("Why, hello there " + str);
