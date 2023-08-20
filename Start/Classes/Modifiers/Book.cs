ccf2e6cb21a1359c81d25055e77cd270fb57138e
Dat T. Tran <datttrian@gmail.com>
2023-08-19 20:04:19 -0400

    Access modifiers

---
+++ Start/Classes/Modifiers/Book.cs
@@ -0,0 +1,66 @@
+﻿namespace Modifiers
+{
+    // access modifiers control how properties and methods are accessed
+    class Book
+    {
+        // TODO: "public" members and methods can be accessed by any other code
+        // Note: this is *NOT* the right way to expose internal data
+        public string _name;
+
+        public string get_name()
+        {
+            return this._name;
+        }
+
+        public void set_name(string _name)
+        {
+            this._name = _name;
+        }
+
+
+        // TODO: "protected" members can only be accessed by the class or a
+        // derived class from this one
+        protected string _author;
+
+        public string get_author()
+        {
+            return this._author;
+        }
+
+        public void set_author(string _author)
+        {
+            this._author = _author;
+        }
+
+
+        // TODO: "private" is the default and can only be accessed by code
+        // within the class itself
+        private int _pagecount;
+
+        public int get_pagecount()
+        {
+            return this._pagecount;
+        }
+
+        public void set_pagecount(int _pagecount)
+        {
+            this._pagecount = _pagecount;
+        }
+
+
+        public Book(string name, string author, int pages)
+        {
+            _name = name;
+            _author = author;
+            _pagecount = pages;
+        }
+
+        public string GetDescription()
+        {
+            return $"{_name} by {_author}, {_pagecount} pages";
+        }
+
+        // TODO: Member variables can be accessed via methods
+
+    }
+}
---
+++ Start/Classes/Modifiers/Program.cs
@@ -0,0 +1,14 @@
+﻿using Modifiers;
+
+// create a new Book instance
+Book b1 = new Book("War and Peace", "Leo Tolstoy", 825);
+
+b1._name = "Aldous Huxley";
+Console.WriteLine(b1.GetDescription());
+
+// set data using functions instead of accessing fields directly
+b1.set_name("Grapes of Wrath");
+b1.set_author("John Steinbeck");
+b1.set_pagecount(464);
+
+Console.WriteLine(b1.GetDescription());
